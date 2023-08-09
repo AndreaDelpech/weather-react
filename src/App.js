a8fac9b2dd06910c8b27ced786158565c522c6a0
Andrea Delpech <delpech.andrea@gmail.com>
2023-08-09 13:56:43 -0700

    Added link to Github

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
       </header>
+      <footer className="App-footer">
+        This project was coded by Andrea Delpech and is{" "}
+        <a
+          href="https://github.com/AndreaDelpech/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
