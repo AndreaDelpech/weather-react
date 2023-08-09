447481993c204e1f9a2a2dc547c823f99583bf57
Andrea Delpech <delpech.andrea@gmail.com>
2023-08-09 16:11:34 -0700

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-      </header>
-      <footer className="App-footer">
-        This project was coded by Andrea Delpech and is{" "}
-        <a
-          href="https://github.com/AndreaDelpech/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>Weather App</h1>
+        </header>
+
+        <footer className="App-footer">
+          This project was coded by Andrea Delpech and is{" "}
+          <a
+            href="https://github.com/AndreaDelpech/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Search.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default function Search() {
-  function handleResponse(response) {
-    alert(`The weather in Paris is ${response.data.main.temp} °C`);
-  }
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=64469ac67e6dc941feb5b50915a18dc7&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-}
