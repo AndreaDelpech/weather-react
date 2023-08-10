2c71fa8ff701c876072e7b1e3df048b078778d2a
Andrea Delpech <delpech.andrea@gmail.com>
2023-08-09 17:20:14 -0700

    HTML base of project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <h1>Weather App</h1>
-        </header>
+        <Weather />
 
         <footer className="App-footer">
           This project was coded by Andrea Delpech and is{" "}
